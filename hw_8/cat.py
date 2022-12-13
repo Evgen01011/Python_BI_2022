c9371348b210384f9d1b4ffa0f4656c05a21060a
Evgen01011 <113452497+Evgen01011@users.noreply.github.com>
2022-12-13 10:13:44 +0300

    Add comments

--- hw_8/cat.py
+++ hw_8/cat.py
@@ -5,6 +5,8 @@ import os
 import shutil
 import argparse
 
+# create arguments - files for reading
+
 parser = argparse.ArgumentParser(
                     prog='cat analog',
                     description='Program read file',
@@ -14,14 +16,16 @@ parser.add_argument('files', nargs='*')
 
 args = parser.parse_args()
 
+# read from stdin in the case of files absence 
 if len(args.files) == 0:
     for line in sys.stdin:
         print(line.strip())
+# read from files
 else:
     for files in args.files:
-        if not os.path.exists(files):
+        if not os.path.exists(files):                          # check files existence
             print(files, ': No such file or directory')
         else:
             with open(files, 'r') as reading:
-                for line in reading:
+                for line in reading:                           # line by line in stdout
                     print(line.strip())
