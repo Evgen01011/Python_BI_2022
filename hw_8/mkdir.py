32454e66b026d1495056355c65568f972e831504
Evgen01011 <113452497+Evgen01011@users.noreply.github.com>
2022-12-13 10:25:10 +0300

    Add comments

--- hw_8/mkdir.py
+++ hw_8/mkdir.py
@@ -5,6 +5,8 @@ import os
 import shutil
 import argparse
 
+# create arguments - directory and flag for parent directories
+
 parser = argparse.ArgumentParser(
                     prog='mkdir analog',
                     description='Program create directory',
@@ -16,14 +18,14 @@ parser.add_argument('directories', nargs='+')
 args = parser.parse_args()
 
 for directories in args.directories:
-    if args.parents:
+    if args.parents:                                                       # for directory with intermediate directories
         creation = os.path.join(os.getcwd(), directories)
         os.makedirs(creation)
     else:
         try: 
-            creation = os.path.join(os.getcwd(), directories)
+            creation = os.path.join(os.getcwd(), directories)              # for subsequent directory
             os.mkdir(creation)
         except:
-            print('No way - mkdir: cannot create directory', directories)
+            print('No way - mkdir: cannot create directory', directories)  # exception if user forgot flags
 
 
