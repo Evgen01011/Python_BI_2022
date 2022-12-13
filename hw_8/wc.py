4bb4db0df48d379ded99e51b85452537357d396a
Evgen01011 <113452497+Evgen01011@users.noreply.github.com>
2022-12-13 11:25:53 +0300

    Change output and add comments

--- hw_8/wc.py
+++ hw_8/wc.py
@@ -4,6 +4,8 @@ import sys
 import os
 import argparse
 
+# create arguments - file and flags
+
 parser = argparse.ArgumentParser(
                     prog='wc analog',
                     description='Program returns statistics of contains',
@@ -17,52 +19,53 @@ parser.add_argument('files', nargs='*')
 args = parser.parse_args()
 
 output = [0, 0, 0]
-switch = 1
 current_list = []
 
+# wc from stdin
 if len(args.files) == 0:
     for line in sys.stdin:
         output[0] += 1
         output[1] += len(line)
         output[2] += len(line.encode('utf-8'))
+# wc from files        
 else:
     for file in args.files:
-        if not os.path.exists(file):
+        if not os.path.exists(file):                                # check file existence 
             print(file, ': No such file or directory')
         else:
             line_number = 0
             word_number = 0
             bites_number = 0
             with open(file, 'r') as reading:
-                for line in reading.readlines():
+                for line in reading.readlines():                    # collect statistics by every line
                     line_number += 1
                     word_number += len(line.split())
                     bites_number += len(line.encode('utf-8'))
-                output[0] += line_number
+                output[0] += line_number                            # collect total statistics
                 output[1] += word_number
                 output[2] += bites_number
+                
+                # different outpyt option
                 if {args.line, args.words, args.bytes} <= {0}:
                     print(line_number, word_number, bites_number, file)
                 else: 
-                    if args.line is True:
-                        current_list.append(line_number)
+                    # determine every flags
+                    if args.line is True: 
+                        print(line_number, end=' ')
                     if args.words is True:
-                        current_list.append(word_number)
+                        print(word_number, end=' ')
                     if args.bytes is True:
-                        current_list.append(bites_number)
-                    print(*current_list, file)
-                    current_list = []
-
-ultimate_list = []
+                        print(bites_number, end=' ')
+                    print(file)
+                    
+# total output
 if {args.line, args.words, args.bytes} <= {0}:
     print(*output, 'Total')
 else:
     if args.line is True:
-        ultimate_list.append(output[0])
+        print(output[0], end=' ')
     if args.words is True:
-        ultimate_list.append(output[1])
+        print(output[1], end=' ')
     if args.bytes is True:
-        ultimate_list.append(output[2])
-
-if len(ultimate_list) > 0:
-    print(*ultimate_list, 'Total')
+        print(output[2], end=' ')
+    print('Total')
