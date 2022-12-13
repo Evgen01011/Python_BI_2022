1470a948eba0f86bde1ec7e038aa9d85f6c723d3
Evgen01011 <113452497+Evgen01011@users.noreply.github.com>
2022-12-13 10:27:20 +0300

    Add comments

--- hw_8/rm.py
+++ hw_8/rm.py
@@ -5,6 +5,9 @@ import os
 import shutil
 import argparse
 
+
+# create arguments - file/ directory and flag for remove directories
+
 parser = argparse.ArgumentParser(
                     prog='rm analog',
                     description='Program remove file or directory',
@@ -15,15 +18,15 @@ parser.add_argument('-r', '--recursive', action='store_true')
 
 args = parser.parse_args()
 
-if len(args.directories) == 0:
+if len(args.directories) == 0:                                                        # check attendence of argument
     print('rm: missing operand')
 else:
     for directories in args.directories:
-        if not os.path.exists(directories):
+        if not os.path.exists(directories):                                           # check file existence
             print('rm: cannot remove ', directories, ': No such file or directory')
-        elif os.path.isfile(directories):
+        elif os.path.isfile(directories):                                             # remove files
             os.remove(directories)
-        elif os.path.isdir(directories) and args.recursive:
+        elif os.path.isdir(directories) and args.recursive:                           # remove directories
             shutil.rmtree(directories)
         else:
-            print('rm: cannot remove ', directories, ': Is a directory')
+            print('rm: cannot remove ', directories, ': Is a directory')              # warning if user forgot flag
